fecbc82cb88f1442fa7f6e6a839016ae29ed319e
Kyle Tower <kyle.tower@gmail.com>
2026-01-23 17:21:32 -0500

    Add events through 2023

--- src/data/events.js
+++ src/data/events.js
@@ -4,342 +4,419 @@ export const events = [
     title: "Alicia Ostein & Matt Bowman Wedding",
     date: "September 26, 2010",
     location: "Mount Pisgah UMC / Swift Creek Baptist",
-    description: "Ceremony and Reception."
+    description: "Ceremony and Reception.",
   },
   {
     id: 20101022,
     title: "DJ Youth Group with Blanton",
     date: "October 22, 2010",
     location: "Richmond, VA",
-    description: "Youth Group Event."
+    description: "Youth Group Event.",
   },
   {
     id: 20110424,
     title: "Josh Griffin & Zach Crump Birthday",
     date: "April 24, 2011",
     location: "Richmond, VA",
-    description: "Birthday Celebration."
+    description: "Birthday Celebration.",
   },
   {
     id: 20120421,
     title: "Susan & Josh's Birthday / House Show",
     date: "April 21, 2012",
     location: "Richmond, VA",
-    description: "[ nameless ] house show and birthday party."
+    description: "[ nameless ] house show and birthday party.",
   },
   {
     id: 20120526,
     title: "Brittany & Kris (KO) Wedding",
     date: "May 26, 2012",
     location: "Bridgewater, VA",
-    description: "Wedding Celebration."
+    description: "Wedding Celebration.",
   },
   {
     id: 20130713,
     title: "Christina & Paul Eiker Wedding",
     date: "July 13, 2013",
     location: "Richmond, VA",
-    description: "Wedding Celebration."
+    description: "Wedding Celebration.",
   },
   {
     id: 20140124,
     title: "Kristin & Julian Cha Wedding",
     date: "January 24, 2014",
     location: "Lakeside Presbyterian Church",
-    description: "Ceremony and Reception."
+    description: "Ceremony and Reception.",
   },
   {
     id: 20150117,
     title: "Christy Plumly & Daniel Graves Wedding",
     date: "January 17, 2015",
     location: "Bolling Haxall House",
-    description: "Wedding Reception."
+    description: "Wedding Reception.",
   },
   {
     id: 20150509,
     title: "Sarah Hustead & Andrew Campbell Wedding",
     date: "May 9, 2015",
     location: "Pole Green Historical Church",
-    description: "Mechanicsville, VA."
+    description: "Mechanicsville, VA.",
   },
   {
     id: 20160312,
     title: "Sigourney & Max Gruenther Wedding",
     date: "March 12, 2016",
     location: "Bon Air Christian Church",
-    description: "Wedding Ceremony."
+    description: "Wedding Ceremony.",
   },
   {
     id: 20161104,
     title: "Laura Traylor & Logan Meade Wedding",
     date: "November 4, 2016",
     location: "Havana 59",
-    description: "Wedding Reception."
+    description: "Wedding Reception.",
   },
   {
     id: 20170616,
     title: "Dee Dee’s 39th Birthday",
     date: "June 16, 2017",
     location: "Swansboro (Manchester), Richmond, VA",
-    description: "Birthday Celebration."
+    description: "Birthday Celebration.",
   },
   {
     id: 20170714,
     title: "Elaine Horjus 50th Birthday",
     date: "July 14, 2017",
     location: "The Boathouse at Rocketts Landing",
-    description: "Birthday Celebration overlooking the river."
+    description: "Birthday Celebration overlooking the river.",
   },
   {
     id: 20170805,
     title: "Cassidy Shoun & Randall Mailand Wedding",
     date: "August 5, 2017",
     location: "The Boathouse at Sunday Park",
-    description: "Wedding Celebration."
+    description: "Wedding Celebration.",
   },
   {
     id: 20171014,
     title: "Collegiate Odd Ball Dance",
     date: "October 14, 2017",
     location: "Collegiate School",
-    description: "High School Dance."
+    description: "High School Dance.",
   },
   {
     id: 20171021,
     title: "Jenny & Kyle Archuleta Wedding",
     date: "October 21, 2017",
     location: "Richmond, VA",
-    description: "Wedding Celebration."
+    description: "Wedding Celebration.",
   },
   {
     id: 20171028,
     title: "Joell Maisano & James Denison Wedding",
     date: "October 28, 2017",
     location: "Seven Springs, Manquin, VA",
-    description: "A beautiful celebration at a historic venue."
+    description: "A beautiful celebration at a historic venue.",
   },
   {
     id: 20171217,
     title: "Remnant Semi-Formal Christmas Party",
     date: "December 17, 2017",
     location: "Richmond, VA",
-    description: "Holiday Party."
+    description: "Holiday Party.",
   },
   {
     id: 20180310,
     title: "Rachel Elder & Nick Seitz Wedding",
     date: "March 10, 2018",
     location: "Hanover Arts & Activities Center",
-    description: "Wedding Reception."
+    description: "Wedding Reception.",
   },
   {
     id: 20180609,
     title: "Erin Pusey & Jeffrey Alford Jr. Wedding",
     date: "June 9, 2018",
     location: "Hanover Tavern",
-    description: "Wedding Celebration."
+    description: "Wedding Celebration.",
   },
   {
     id: 20181012,
     title: "Karen & Paul May 25th Anniversary",
     date: "October 12, 2018",
     location: "The Hippodrome Theater",
-    description: "Vow Renewal Ceremony and Reception."
+    description: "Vow Renewal Ceremony and Reception.",
   },
   {
     id: 20190101,
     title: "25th Anniversary CHRF Bowl-A-Thon",
     date: "2019",
     location: "Hanover Lanes",
-    description: "Children's Hospital of Richmond Foundation fundraiser."
+    description: "Children's Hospital of Richmond Foundation fundraiser.",
   },
   {
     id: 20190330,
     title: "Bethany & Travis Wedding",
     date: "March 30, 2019",
     location: "Richmond, VA",
-    description: "Wedding Reception."
+    description: "Wedding Reception.",
   },
   {
     id: 20190503,
     title: "9th Annual Connor's Heroes Art Ball",
     date: "May 3, 2019",
     location: "Redskins Training Center",
-    description: "Fundraiser Event."
+    description: "Fundraiser Event.",
   },
   {
     id: 20190518,
     title: "VCU Health Hematology/Oncology Prom",
     date: "May 18, 2019",
     location: "Richmond, VA",
-    description: "Prom for patients."
+    description: "Prom for patients.",
   },
   {
     id: 20190727,
     title: "Summer Night Lights",
     date: "July 27, 2019",
     location: "Richmond, VA",
-    description: "Community Event."
+    description: "Community Event.",
   },
   {
     id: 20190907,
     title: "Emily & Ethan Bynum Wedding",
     date: "September 7, 2019",
     location: "Virginia Historical Museum",
-    description: "Featured unique songs never before heard at a wedding reception in our history."
+    description:
+      "Featured unique songs never before heard at a wedding reception in our history.",
   },
   {
     id: 20191012,
     title: "Hannah Clarke & Taylor Maynard Wedding",
     date: "October 12, 2019",
     location: "Grove Avenue Baptist Church",
-    description: "A relaxing reception with an army of six flower girls!"
+    description: "A relaxing reception with an army of six flower girls!",
   },
   {
     id: 20191100,
     title: "ASK 5K Donut Run",
     date: "November 2019",
     location: "St. Christopher’s School",
-    description: "Fundraiser Run."
+    description: "Fundraiser Run.",
   },
   {
     id: 20200222,
     title: "26th Anniversary CHRF Bowl-A-Thon",
     date: "February 22, 2020",
     location: "Hanover Lanes",
-    description: "Raised over $45,000 for pediatric cancer research."
+    description: "Raised over $45,000 for pediatric cancer research.",
   },
   {
     id: 20200705,
     title: "Tiffani Willis & Stephen Guenther Wedding",
     date: "July 5, 2020",
     location: "Swift Creek Banquet Hall",
-    description: "Ceremony and Reception."
+    description: "Ceremony and Reception.",
   },
   {
     id: 20200809,
     title: "Taylor Gordan & Cameron Bell Wedding",
     date: "August 9, 2020",
     location: "Richmond, VA",
-    description: "Wedding Reception."
+    description: "Wedding Reception.",
   },
   {
     id: 20201003,
     title: "Erin Coumes & Kyle Miller Wedding",
     date: "October 3, 2020",
     location: "Richmond, VA",
-    description: "Wedding Reception."
+    description: "Wedding Reception.",
   },
   {
     id: 20201101,
     title: "10th Annual ASK 5K Donut Run",
     date: "November 1, 2020",
     location: "St. Christopher’s School",
-    description: "Fundraiser Run."
+    description: "Fundraiser Run.",
   },
   {
     id: 20201211,
     title: "New City Academy Christmas Party",
     date: "December 11, 2020",
     location: "Richmond, VA",
-    description: "Collecting food for FeedMore to help those in need around Central Virginia."
+    description:
+      "Collecting food for FeedMore to help those in need around Central Virginia.",
   },
   {
     id: 20210501,
     title: "Pete Walsh Block Party",
     date: "May 2021",
     location: "Richmond, VA",
-    description: "Neighborhood Block Party."
+    description: "Neighborhood Block Party.",
   },
   {
     id: 20210620,
     title: "Kaleigh Woods & Joseph Spriggs Wedding",
     date: "June 20, 2021",
     location: "Richmond, VA",
-    description: "Wedding Celebration."
+    description: "Wedding Celebration.",
   },
   {
     id: 20210701,
     title: "Pete Walsh Block Party (91st Birthday)",
     date: "July 2021",
     location: "Richmond, VA",
-    description: "Neighborhood Birthday Celebration."
+    description: "Neighborhood Birthday Celebration.",
   },
   {
     id: 20210807,
     title: "Mykaila McWhirt & Malik Malone Wedding",
     date: "August 7, 2021",
     location: "Richmond, VA",
-    description: "Wedding Reception."
+    description: "Wedding Reception.",
   },
   {
     id: 20211016,
     title: "Haley Gunter & Chris Healy Anniversary",
     date: "October 16, 2021",
     location: "Richmond, VA",
-    description: "1 Year Anniversary Celebration."
+    description: "1 Year Anniversary Celebration.",
   },
   {
     id: 20211023,
     title: "ASK Fall Family Festival",
     date: "October 23, 2021",
     location: "Richmond, VA",
-    description: "Family Festival."
+    description: "Family Festival.",
   },
   {
     id: 20211120,
     title: "11th Annual ASK 5K Donut Run",
     date: "November 20, 2021",
     location: "Bon Secours Washington Football Training",
-    description: "Fundraiser Run."
+    description: "Fundraiser Run.",
   },
   {
     id: 20220115,
     title: "Jamie Hall 40th Birthday",
     date: "January 15, 2022",
     location: "Richmond, VA",
-    description: "Great food, great music, great people!"
+    description: "Great food, great music, great people!",
   },
   {
     id: 20220226,
     title: "Heather & PJ Calhoun Baby Shower",
     date: "February 26, 2022",
     location: "Richmond, VA",
-    description: "Celebrating Hazel's fast-approaching arrival."
+    description: "Celebrating Hazel's fast-approaching arrival.",
   },
   {
     id: 20220506,
     title: "11th Annual Connor's Heroes Art Ball",
     date: "May 6, 2022",
     location: "The John Marshall Ballroom",
-    description: "A record-breaking night raising $517,000 for pediatric cancer research."
+    description:
+      "A record-breaking night raising $517,000 for pediatric cancer research.",
   },
   {
     id: 20220827,
     title: "Gwen Rogue & Logan Shoun Wedding",
     date: "August 27, 2022",
     location: "Richmond, VA",
-    description: "Ceremony and Reception."
+    description: "Ceremony and Reception.",
   },
   {
     id: 20221002,
     title: "Kayla DeAlto & Jimmy Foit Wedding",
     date: "October 2, 2022",
     location: "Richmond, VA",
-    description: "Ceremony and Reception."
+    description: "Ceremony and Reception.",
   },
   {
     id: 20221022,
     title: "ASK Fall Family Festival",
     date: "October 22, 2022",
     location: "Richmond, VA",
-    description: "We had a fantastic time at the 2nd Annual ASK Fall Family Festival."
+    description:
+      "We had a fantastic time at the 2nd Annual ASK Fall Family Festival.",
   },
   {
     id: 20221119,
     title: "12th Annual ASK 5K Donut Run",
     date: "November 19, 2022",
     location: "St. Christopher’s School",
-    description: "Fundraiser Run."
-  }
+    description: "Fundraiser Run.",
+  },
+  {
+    id: 20230505,
+    title: "12th Annual Connor's Heroes Art Ball",
+    date: "May 5, 2023",
+    location: "The John Marshall Ballroom",
+    description:
+      "Wonderful night raising over $80,000 for pediatric cancer research.",
+  },
+  {
+    id: 20230513,
+    title: "Ariana Berger & Rudy Pedroza Wedding",
+    date: "May 13, 2023",
+    location: "Avonlea Farms | Mineral, VA",
+    description: "Ceremony and Reception.",
+  },
+  {
+    id: 20230610,
+    title: "Strom Graduation Party",
+    date: "June 10, 2023",
+    location: "Richmond, VA",
+    description: "Graduation Party.",
+  },
+  {
+    id: 20230701,
+    title: "Bria Jacobs & Dawson Maier Wedding",
+    date: "July 1, 2023",
+    location:
+      "Remnant Church Midtown | Glen Allen Cultural Arts Center | Glen Allen, VA",
+    description: "Ceremony and Reception.",
+  },
+  {
+    id: 20230708,
+    title: "Karis Gibson & Daniel Chun Wedding",
+    date: "July 8, 2023",
+    location: "The Historic Hofheimer | Richmond, VA",
+    description: "Ceremony and Reception.",
+  },
+  {
+    id: 20230722,
+    title: "Helen Tower 61st Birthday",
+    date: "July 22, 2023",
+    location: "Richmond, VA",
+    description: "Celebrating 61 years of life with family and friends.",
+  },
+  {
+    id: 20230916,
+    title: "Katherine Hayes & Jackson Prillaman Wedding",
+    date: "September 16, 2023",
+    location: "Historic Jasmine | Providence Forge, VA",
+    description: "Ceremony and Reception.",
+  },
+  {
+    id: 20231020,
+    title: "Blakeney's Adoption Party",
+    date: "October 20, 2023",
+    location: "Richmond, VA",
+    description: "A special night celebrating the Blakeney's adoption.",
+  },
+  {
+    id: 20231028,
+    title: "ASK Fall Family Festial",
+    date: "October 28, 2023",
+    location: "Richmond, VA",
+    description:
+      "A wonderful day at the Washington Commander's Training Center.",
+  },
+  {
+    id: 20231209,
+    title: "ASK Holiday Party",
+    date: "December 9, 2023",
+    location: "Richmond Marriott Downtown | Richmond, VA",
+    description: "Celebrating another successful year of ASK.",
+  },
 ];
