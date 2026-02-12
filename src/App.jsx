82e5c2f94dd0449591ed481bcf642f2146131c55
Kyle Tower <kyle.tower@gmail.com>
2026-02-12 10:24:07 -0500

    Add ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import { Home } from "./pages/Home";
-import { PastEvents } from "./pages/PastEvents";
-import { Inquiry } from "./pages/Inquiry";
-import { Success } from "./pages/Success";
-import { Footer } from "./components/Footer";
-import "./App.css";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { ScrollToTop } from './components/ScrollToTop'; // Added import
+import { Home } from './pages/Home';
+import { PastEvents } from './pages/PastEvents';
+import { Inquiry } from './pages/Inquiry';
+import { Success } from './pages/Success';
+import { Footer } from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
+      <ScrollToTop />
+      <div className='app-container'>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/events" element={<PastEvents />} />
-          <Route path="/inquiry" element={<Inquiry />} />
-          <Route path="/success" element={<Success />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/events' element={<PastEvents />} />
+          <Route path='/inquiry' element={<Inquiry />} />
+          <Route path='/success' element={<Success />} />
         </Routes>
         <Footer />
       </div>
